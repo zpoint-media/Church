5d54a83b53622b0a205b2c7b48c0625e94bfb0d5
zpoint-media <zpointsportmedia@gmail.com>
2026-02-27 20:28:09 +0100

    Revert "Add Cloudflare Workers configuration"

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
